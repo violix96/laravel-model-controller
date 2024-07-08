3119f5f22a4b0ab344a40a27a7f5b53e03cce282
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-07-08 16:04:01 +0200

    [UPD] added movie retrieval and passed data to guest index view

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::all();
+        return view('guest.index', compact('movies'));
+    }
 }
